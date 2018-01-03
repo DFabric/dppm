c85fe7baf6ba7e11f301744bae66664f09765714
Julien Reichardt <mi@jrei.ch>
2018-01-03 16:25:34 +0100

    Add specs

---
+++ spec/owner_spec.cr
@@ -0,0 +1,27 @@
+require "./spec_helper"
+
+describe Owner do
+  it "converts an user name to its id" do
+    Owner.to_id("root", "uid").should eq(0)
+  end
+
+  it "converts a group name to its id" do
+    Owner.to_id("root", "gid").should eq(0)
+  end
+
+  it "returns the passed uid as an Int from a String" do
+    Owner.to_id("0", "uid").should eq(0)
+  end
+
+  it "returns the passed gid as an Int from an Int" do
+    Owner.to_id(0, "gid").should eq(0)
+  end
+
+  it "converts an user id as String to its name" do
+    Owner.from_id("0", "uid").should eq("root")
+  end
+
+  it "converts a group id as Int32 to its name" do
+    Owner.from_id(0, "gid").should eq("root")
+  end
+end
--- spec/utils_spec.cr
+++ spec/utils_spec.cr
@@ -1,28 +1,55 @@
 require "./spec_helper"
 
 describe Utils do
-  describe "chmod_r" do
-    it "changes the permissions of the directory recursively" do
-      path = Dir.current + '/' + "chmod_r_test"
-      begin
-        Dir.mkdir(path)
-        Dir.mkdir(File.join(path, "a"))
-        File.write(File.join(path, "a/b"), "")
+  it "changes the permissions of the directory recursively" do
+    path = Dir.current + "/chmod_r_test"
+    begin
+      Dir.mkdir path
+      Dir.mkdir File.join(path, "a")
+      File.write File.join(path, "a/b"), ""
 
-        Utils.chmod_r(path, 0o775)
-        File.stat(File.join(path, "a")).perm.should eq(0o775)
-        File.stat(File.join(path, "a/b")).perm.should eq(0o775)
-      ensure
-        FileUtils.rm_r(path)
-      end
+      Utils.chmod_r path, 0o775
+      File.stat(File.join path, "a").perm.should eq 0o775
+      File.stat(File.join path, "a/b").perm.should eq 0o775
+    ensure
+      FileUtils.rm_r(path)
     end
   end
 
-  describe "chown_r" do
-    it "change the owner of the directory recursively" do
-      # changing owners requires special privileges, so we test that method calls do compile
-      typeof(Utils.chown_r("/tmp/test"))
-      typeof(Utils.chown_r("/tmp/test", uid: 1001, gid: 100, follow_symlinks: true))
+  it "changes the owner of the directory recursively" do
+    # changing owners requires special privileges, so we test that method calls do compile
+    typeof(Utils.chown_r "/tmp/test")
+    typeof(Utils.chown_r("/tmp/test", uid: 1001, gid: 100, follow_symlinks: true))
+  end
+
+  it "converts to an array" do
+    Utils.to_array("a.b.c").should eq ["a", "b", "c"]
+  end
+
+  describe "String to_type" do
+    it "converts a quoted string to an unquoted one" do
+      Utils.to_type("\"string\"", true).should eq "string"
+    end
+    it "returns the passed string" do
+      Utils.to_type("string", false).should eq "string"
+    end
+    it "converts to a `true` bool" do
+      Utils.to_type("true").should eq true
+    end
+    it "converts to a `false` bool" do
+      Utils.to_type("false").should eq false
+    end
+    it "converts to an empty hash" do
+      Utils.to_type("{}").should eq Hash(String, String).new
+    end
+    it "converts to an empty array" do
+      Utils.to_type("[]").should eq Array(String).new
+    end
+    it "converts to an Int64" do
+      Utils.to_type("72").should eq 72
+    end
+    it "converts to a Float64" do
+      Utils.to_type("1.3").should eq 1.3
     end
   end
 end
