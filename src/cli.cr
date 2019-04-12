79f13c345ce611e65a53b360ab3a5a9c2a222e0b
Julien Reichardt <mi@jrei.ch>
2019-04-12 11:26:03 +0200

    Split the CLI creation from the module

--- src/cli.cr
+++ src/cli.cr
@@ -409,17 +409,3 @@ module CLI
     confirm_prompt { abort "cancelled." }
   end
 end
-
-# TODO: integrate the DPPM API repository
-def server(**args)
-  Log.output.puts "available soon! (press CTRL+C)"
-  sleep
-end
-
-# TODO: change to `dppm api`
-CLI.run(
-  server: {
-    info:   "Start the dppm API server",
-    action: "server",
-  }
-)
---
+++ src/dppm_cli.cr
@@ -0,0 +1,3 @@
+require "./cli"
+
+CLI.run
