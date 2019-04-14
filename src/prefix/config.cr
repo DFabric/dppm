a8872ab9bb67c49ff2f32d876746085cb9554c61
Julien Reichardt <mi@jrei.ch>
2019-04-14 18:59:23 +0200

    Cast port to Int32

--- src/prefix/config.cr
+++ src/prefix/config.cr
@@ -1,7 +1,7 @@
 struct Prefix::Config
   getter sources : Hash(String, String) { data["sources"].as_h.transform_values &.as_s }
   getter host : String { data["host"].as_s }
-  getter port : String { data["port"].as_s }
+  getter port : Int32 { data["port"].as_i }
   getter data : CON::Any
 
   def initialize(content : String)
